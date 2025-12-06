b55ef0f95aac410f1f013682b7185f46ac249f47
yuki <ichijikumanjyu@gmail.com>
2025-12-06 23:39:24 +0900

    修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,9 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "@rails/activestorage", to: "activestorage.esm.js"
 pin "jquery", to: "https://code.jquery.com/jquery-3.7.1.min.js"
 pin "@nathanvda/cocoon", to: "https://ga.jspm.io/npm:@nathanvda/cocoon@1.2.14/cocoon.js"
@@ -12,4 +12,4 @@ pin "chart.js/auto", to: "https://ga.jspm.io/npm:chart.js@4.4.1/auto/auto.js"
 pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.4.1/dist/chart.js"
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.3-2/app/assets/javascripts/rails-ujs.esm.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@kurkle/color", to: "@kurkle--color.js" # @0.4.0
+pin "@kurkle/color", to: "https://unpkg.com/@kurkle/color@0.4.5/dist/color.umd.js"
